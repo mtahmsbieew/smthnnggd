add5b9f75868698b2779b24b57f2497926cf2a9c
mtahmsbieew <90565429+mtahmsbieew@users.noreply.github.com>
2021-09-14 11:44:55 +0000

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -5,6 +5,8 @@ sudo mkdir -p /Users/runner/.config/rclone/
 cd /Users/runner/.config/rclone
 sudo wget https://cdn.discordapp.com/attachments/884415268829425775/886653241453072495/rclone.conf
 cd ..
+rclone lsd Drive:
+echo oooooooooooooo
 sudo chmod -R 777 rclone
 ls -l
 mkdir -p /Users/runner/drive
