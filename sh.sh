a2461ac1e06b2b1089f79208ce7fcdb25d5fce39
mtahmsbieew <90565429+mtahmsbieew@users.noreply.github.com>
2021-09-14 06:56:41 +0000

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -1,7 +1,7 @@
 cd /Applications
 #rm -rf Xcode_12.app Xcode_12.4.app Xcode_12.3.app
 curl https://rclone.org/install.sh --http1.1 | bash -s beta
-rclone config
+mkdir -p /Users/runner/.config/rclone/
 cd /Users/runner/.config/rclone/
 wget https://cdn.discordapp.com/attachments/884415268829425775/886653241453072495/rclone.conf
 mkdir -p /content/drive
