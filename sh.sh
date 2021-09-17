b44793676a37419f4cfd6d2125d72fdd55a40ca2
mtahmsbieew <90565429+mtahmsbieew@users.noreply.github.com>
2021-09-17 16:59:19 +0000

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -20,7 +20,7 @@ cd ..
 sudo chmod -R 777 drive
 rclone lsd remote:
 cd Documents
-rclone mount --daemon --allow-root --log-file /Users/runner/log.txt --log-level DEBUG remote: /Users/runner/Documents
+rclone mount --daemon --allow-other --log-file /Users/runner/log.txt --log-level DEBUG remote: /Users/runner/Documents
 
 #touch olalalalalalalalalala
 ls -al
