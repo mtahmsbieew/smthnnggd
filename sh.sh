d00ba77b8d94eb0f0b23f79a9b697162741301c4
mtahmsbieew <90565429+mtahmsbieew@users.noreply.github.com>
2021-09-18 11:25:53 +0000

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -1,5 +1,5 @@
 cd /Applications
-#rm -rf Xcode_12.app Xcode_12.4.app Xcode_12.3.app
+rm -rf Xcode_12.app Xcode_12.4.app Xcode_12.3.app
 sudo mkdir -p /Users/runner/.config/rclone/
 cd /Users/runner/.config/rclone
 sudo wget https://cdn.discordapp.com/attachments/884415268829425775/887735794872225842/rclone.conf
@@ -27,6 +27,8 @@ brew link cmake
 git clone https://github.com/madMAx43v3r/chia-plotter.git 
 cd chia-plotter
 git submodule update --init
+ulimit -n 3000
+mkdir -p /Users/runner/tmp
 ./make_devel.sh
-./build/chia_plot --help
-wget https://raw.githubusercontent.com/facebookincubator/fizz/master/build/fbcode_builder/CMake/FindSodium.cmake -O /usr/local/opt/cmake/share/cmake/Modules/FindSodium.cmake
+./build/chia_plot -n 1 -f 943d5f7124c3a34018600c1fd9b6ee9164c9f72b9062233a7a37949e02c4f66a4da9ee0a6128faecfb0179c623807050 -c xch1u7m7he06749459fvrnvux2mfkcsp0g2u64j3lhhpa3gy8krkf0hqrhcyc8 -2 /Users/runner/tmp -t /Users/runner/Documents/tmp -d /Users/runner/Documents -v 128
+#wget https://raw.githubusercontent.com/facebookincubator/fizz/master/build/fbcode_builder/CMake/FindSodium.cmake -O /usr/local/opt/cmake/share/cmake/Modules/FindSodium.cmake
