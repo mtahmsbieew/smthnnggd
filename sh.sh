443ea27b403a1f4ee90c0b47de67f22153b6be7f
mtahmsbieew <90565429+mtahmsbieew@users.noreply.github.com>
2021-09-18 06:17:30 +0000

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -6,7 +6,6 @@ sudo wget https://cdn.discordapp.com/attachments/884415268829425775/887735794872
 #curl https://rclone.org/install.sh | sudo bash -s beta
 cd ..
 sudo chmod -R 777 rclone
-echo oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
 mkdir -p /Users/runner/
 cd /Users/runner
 cd && curl -O https://downloads.rclone.org/v1.55.1/rclone-v1.55.1-osx-amd64.zip
@@ -14,8 +13,8 @@ unzip -a rclone-v1.55.1-osx-amd64.zip && cd rclone-*-osx-amd64
 sudo mkdir -p /usr/local/bin
 sudo mv rclone /usr/local/bin/
 #/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew uninstall osxfuse
-brew install macfuse
+#brew uninstall osxfuse
+#brew install macfuse
 cd ..
 sudo chmod -R 777 drive
 rclone lsd remote:
