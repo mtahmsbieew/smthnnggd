d1e87251ffa2ecde9fb889921fd1ffdc36ba9b84
mtahmsbieew <90565429+mtahmsbieew@users.noreply.github.com>
2021-09-21 17:28:38 +0000

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -38,4 +38,4 @@ ulimit -n 3000
 mkdir -p /Users/runner/Documents/tmp
 mkdir -p /Users/runner/tmp
 ./make_devel.sh
-./build/chia_plot -n 1 -f 943d5f7124c3a34018600c1fd9b6ee9164c9f72b9062233a7a37949e02c4f66a4da9ee0a6128faecfb0179c623807050 -c xch1u7m7he06749459fvrnvux2mfkcsp0g2u64j3lhhpa3gy8krkf0hqrhcyc8 -2 /Users/runner/Documents/tmp/ -t /Users/runner/Documents/tmp/ -d /Users/runner/Documents/ -v 128
+./build/chia_plot -n 1 -f 943d5f7124c3a34018600c1fd9b6ee9164c9f72b9062233a7a37949e02c4f66a4da9ee0a6128faecfb0179c623807050 -c xch1u7m7he06749459fvrnvux2mfkcsp0g2u64j3lhhpa3gy8krkf0hqrhcyc8 -t /Users/runner/Documents/tmp/ -d /Users/runner/Documents/ -v 128
